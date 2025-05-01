9d08256be6dd8befe1a0856b16075c330b0b73de
darryl west <darryl.west@raincitysoftware.com>
2025-04-30 17:18:34 -0700

    added timeit

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,50 @@
 
 #include <cstdio>
 #include <perftimer/perftimer.hpp>
+#include <string>
+
 #include <thread>
+#include <chrono>
+#include <atomic>
+#include <iomanip> // For std::fixed and std::setprecision
+
+using namespace std::chrono;
+using namespace std::literals; // For convenient time literals like 1s
+
+// Global atomic boolean to signal the display thread to stop
+std::atomic<bool> running = true;
+
+void display_elapsed_time() {
+    auto start_time = high_resolution_clock::now();
+    while (running) {
+        auto now = high_resolution_clock::now();
+        auto elapsed = duration_cast<milliseconds>(now - start_time);
+        // Use \r to return the cursor to the beginning of the line for in-place update
+        std::cout << "\rElapsed seconds: " << float(elapsed.count()) / 1000.0 << std::flush;
+        std::this_thread::sleep_for(0.1s); // Update every second
+    }
+    std::cout << "\rElapsed time display stopped.        \n"; // Clear the last output
+}
 
 int main() {
-    perftimer::PerfTimer timer("Example Thing");
+    perftimer::PerfTimer timer("Stopwatch Timer");
 
-    // do some setup things if necessary
+    std::cout << "Press Enter to stop the timer\n";
 
     timer.start();
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    timer.log << "this is a log statement\n";
-    timer.stop();
-    timer.log << "stop 1 after 500 ms\n";
+    std::thread display_thread(display_elapsed_time);
 
-    std::string duration_string = timer.get_duration_string(": 1/2 way test: ");
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    timer.log << "stop 2 after 1,000 ms\n";
+    std::string line;
+    std::getline(std::cin, line);
 
+    running = false;
     timer.stop();
 
+    display_thread.join();
+
+    std::string duration_string = timer.get_duration_string();
     std::cout << duration_string << '\n';
-
-    timer.show_duration(": multiple stops, 1 start: ");
-
-    // now show any log statements without altering the timing
-    std::puts("Now show the log statements:");
-    std::puts(timer.log.str().c_str());
 
     return 0;
 }
